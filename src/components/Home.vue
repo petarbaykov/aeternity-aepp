12c911daffd52be08d628639f7be6e3c41c13e7d
Petar Baykov <petbaik26@gmail.com>
2019-12-04 11:04:11 +0200

    Update Aepp example with new SDK version

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -7,19 +7,11 @@
         <div class="p-2 w-1/4">
           Public Key <small>(from Wallet Aepp)</small>
         </div>
-        <div v-if="pub" class="p-2 w-3/4 bg-grey-lightest break-words">
-          {{pub}}
+        <div v-if="addressResponse" class="p-2 w-3/4 bg-grey-lightest break-words">
+          {{addressResponse | responseToString}}
         </div>
         <div v-else class="p-2 w-3/4 bg-grey-lightest break-words text-grey">
           Requesting Public Key from AE Wallet...
-        </div>
-      </div>
-      <div v-if="height" class="bg-green w-full flex flex-row font-mono border border-b">
-        <div class="p-2 w-1/4">
-          Height
-        </div>
-        <div class="p-2 w-3/4 bg-grey-lightest">
-          {{height}}
         </div>
       </div>
       <div v-if="balance" class="bg-green w-full flex flex-row font-mono border border-b">
@@ -30,15 +22,52 @@
           {{balance}}
         </div>
       </div>
-      <div v-if="walletName" class="bg-green w-full flex flex-row font-mono border border-b">
+      <div v-if="heightResponse" class="bg-green w-full flex flex-row font-mono border border-b">
         <div class="p-2 w-1/4">
-          Wallet
+          Height
         </div>
         <div class="p-2 w-3/4 bg-grey-lightest">
-          {{walletName}}
+          {{heightResponse | responseToString}}
         </div>
       </div>
-      <button v-if="walletName" class="w-32 rounded rounded-full bg-purple text-white py-2 px-4 pin-r mr-8 mt-4 text-xs" @click="disconnect">Disconnect</button>
+
+      <template v-if="nodeInfoResponse">
+        <div v-if="nodeInfoResponse.error" class="bg-green w-full flex flex-row font-mono border border-b">
+          <div class="p-2 w-1/4">
+            NodeInfo error
+          </div>
+          <div class="p-2 w-3/4 bg-grey-lightest break-words">
+            {{nodeInfoResponse.error}}
+          </div>
+        </div>
+        <div
+          v-for="(value, name) in nodeInfoResponse.result"
+          v-if="['url', 'name', 'nodeNetworkId', 'version'].includes(name)"
+          class="bg-green w-full flex flex-row font-mono border border-b"
+        >
+          <div class="p-2 w-1/4 capitalize">
+            {{name.replace('nodeNetworkId', 'NetworkId')}}
+          </div>
+          <div class="p-2 w-3/4 bg-grey-lightest">
+            {{value}}
+          </div>
+        </div>
+      </template>
+
+      <div v-if="compilerVersionResponse" class="bg-green w-full flex flex-row font-mono border border-b">
+        <div class="p-2 w-1/4">
+          Compiler version
+        </div>
+        <div class="p-2 w-3/4 bg-grey-lightest">
+          {{compilerVersionResponse | responseToString}}
+        </div>
+      </div>
+
+      <button
+        v-if="addressResponse"
+        class="w-32 rounded rounded-full bg-purple text-white py-2 px-4 pin-r mr-8 mt-4 text-xs"
+        @click="disconnect"
+      >Disconnect</button>
     </div>
 
     <h2 class="mt-4">Spend tokens</h2>
@@ -87,14 +116,14 @@
       </button>
     </div>
 
-    <div v-if="spendResult || spendError" class="border mt-4 mb-8 rounded">
+    <div v-if="spendResponse" class="border mt-4 mb-8 rounded">
       <div class="bg-green w-full flex flex-row font-mono border border-b">
         <div class="p-2 w-1/4">
           Send result
         </div>
         <div
           class="p-2 w-3/4 bg-grey-lightest break-words whitespace-pre-wrap"
-        >{{ spendResult ? JSON.stringify(spendResult, null, 4) : spendError }}</div>
+        >{{ spendResponse | responseToFormattedJSON }}</div>
       </div>
     </div>
 
@@ -114,44 +143,52 @@
       </button>
     </div>
 
-    <div v-if="byteCode || compileError" class="border mt-4 mb-8 rounded">
+    <div v-if="compileBytecodeResponse" class="border mt-4 mb-8 rounded">
       <div class="bg-green w-full flex flex-row font-mono border border-b">
         <div class="p-2 w-1/4">
           Compiled Code
         </div>
         <div class="p-2 w-3/4 bg-grey-lightest break-words">
-          {{ byteCode || compileError }}
+          {{ compileBytecodeResponse | responseToString }}
         </div>
       </div>
     </div>
 
-    <button v-if="byteCode" class="w-32 rounded rounded-full bg-purple text-white py-2 px-4 pin-r mr-8 mt-4 text-xs" @click="deploy">
+    <button
+      v-if="compileBytecodeResponse && compileBytecodeResponse.result"
+      class="w-32 rounded rounded-full bg-purple text-white py-2 px-4 pin-r mr-8 mt-4 text-xs"
+      @click="deploy"
+    >
       Deploy
     </button>
 
-    <div v-if="deployInfo || deployError" class="border mt-4 mb-8 rounded">
+    <div v-if="deployResponse" class="border mt-4 mb-8 rounded">
       <div class="bg-green w-full flex flex-row font-mono border border-b">
         <div class="p-2 w-1/4">
           Deployed Contract
         </div>
         <div
           class="p-2 w-3/4 bg-grey-lightest break-words whitespace-pre-wrap"
-        >{{ deployInfo ? JSON.stringify(deployInfo, null, 4) : deployError }}</div>
+        >{{ deployResponse | responseToFormattedJSON }}</div>
       </div>
     </div>
 
-    <button v-if="deployInfo" class="w-32 rounded rounded-full bg-purple text-white py-2 px-4 pin-r mr-8 mt-4 text-xs" @click="call">
+    <button
+      v-if="deployResponse && deployResponse.result"
+      class="w-32 rounded rounded-full bg-purple text-white py-2 px-4 pin-r mr-8 mt-4 text-xs"
+      @click="call"
+    >
       Call
     </button>
 
-    <div v-if="callResult || callError" class="border mt-4 mb-8 rounded">
+    <div v-if="callResponse" class="border mt-4 mb-8 rounded">
       <div class="bg-green w-full flex flex-row font-mono border border-b">
         <div class="p-2 w-1/4">
           Call Result
         </div>
         <div
           class="p-2 w-3/4 bg-grey-lightest break-words whitespace-pre-wrap"
-        >{{ callResult ? JSON.stringify(callResult, null, 4) : callError }}</div>
+        >{{ callResponse | responseToFormattedJSON }}</div>
       </div>
     </div>
   </div>
@@ -162,110 +199,130 @@
   import { RpcAepp } from '@aeternity/aepp-sdk/es'
   import Detector from '@aeternity/aepp-sdk/es/utils/aepp-wallet-communication/wallet-detector'
   import BrowserWindowMessageConnection from '@aeternity/aepp-sdk/es/utils/aepp-wallet-communication/wallet-connection/browser-window-message'
+  import { isValidKeypair } from '../../../../../../../es/utils/crypto'
+
   // Send wallet connection info to Aepp throug content script
   const NODE_URL = 'https://sdk-testnet.aepps.com'
   const NODE_INTERNAL_URL = 'https://sdk-testnet.aepps.com'
-  
   const COMPILER_URL = 'https://compiler.aepps.com'
+
+  const errorAsField = async fn => {
+    try {
+      return { result: await fn }
+    } catch (error) {
+      console.log(error)
+      return { error }
+    }
+  }
+
   export default {
     data () {
       return {
         runningInFrame: window.parent !== window,
         client: null,
-        height: null,
-        pub: null,
+        addressResponse: null,
+        heightResponse: null,
+        compilerVersionResponse: null,
+        nodeInfoResponse: null,
         spendTo: null,
         spendAmount: null,
         spendPayload: null,
+        spendResponse: null,
         spendResult: null,
         spendError: null,
         balance: null,
         contractCode: `contract Identity =
-      type state = ()
-      function main(x : int) = x`,
+      entrypoint main(x : int) = x`,
         byteCode: null,
-        compileError: null,
+        compileBytecodeResponse: null,
         contractInitState: [],
-        deployInfo: null,
-        deployError: null,
-        callResult: null,
-        callError: null,
-        walletName: null
+        deployResponse: null,
+        callResponse: null,
+        walletName: null,
+        onAccount: null,
+        accounts: []
       }
+    },
+    filters: {
+      responseToString: response => `${response.error ? 'Error: ' : ''}${response.result || response.error}`,
+      responseToFormattedJSON: response => response.error
+        ? `Error: ${response.error}`
+        : JSON.stringify(response.result, null, 4),
     },
     methods: {
       async spend () {
-        try {
-          this.spendResult = await this.client.spend(
-            this.spendAmount,
-            this.spendTo, {
-              payload: this.spendPayload
-            }
-          )
-        } catch (err) {
-          this.spendError = err
-        }
+        const onAccount = Object.keys(this.accounts.address.connected)[0]
+        this.spendResponse = await errorAsField(this.client.spend(
+          this.spendAmount,
+          this.spendTo, {
+            payload: this.spendPayload,
+            // fee: 1
+            // onAccount: onAccount
+          }
+        ));
       },
       async compile () {
-        this.byteCode = this.compileError = null
-        try {
-          this.byteCode = (await this.client.contractCompile(this.contractCode)).bytecode
-        } catch (err) {
-          this.compileError = err
-        }
+        this.compileBytecodeResponse = await errorAsField(
+          (await this.client.contractCompile(this.contractCode)).bytecode
+        );
       },
       async deploy () {
-        this.deployInfo = this.deployError = null
-        try {
-          this.deployInfo = await this.client.contractDeploy(this.byteCode, this.contractCode, this.contractInitState)
-        } catch (err) {
-          this.deployError = err
-        }
+        this.deployResponse = await errorAsField(this.client.contractDeploy(
+          this.compileBytecodeResponse.result, this.contractCode, this.contractInitState
+        ));
       },
       async call (code, method = 'main', returnType = 'int', args = ['5']) {
-        this.callResult = this.callError = null
-        try {
-          this.callResult = await this.client.contractCall(this.contractCode, this.deployInfo.address, method,  args)
-          Object.assign(
-            this.callResult,
+        this.callResponse = await errorAsField((async () => {
+          const result = await this.client.contractCall(
+            this.contractCode, this.deployResponse.result.address, method,  args
+          )
+          return Object.assign(
+            result,
             { decodedRes: await result.decode(returnType) }
           )
-        } catch (err) {
-          this.callError = err
-        }
+        })())
       },
       async disconnect() {
         await this.client.disconnectWallet()
         this.walletName = null
         this.pub = null
         this.balance = null
+        this.addressResponse = null
         this.scanForWallets()
       },
       async getReverseWindow() {
         const iframe = document.createElement('iframe')
-        iframe.src = 'https://base.aepps.com'
+        iframe.src = prompt('Enter wallet URL', 'http://localhost:9000')
         iframe.style.display = 'none'
         document.body.appendChild(iframe)
         return iframe.contentWindow
       },
       async connectToWallet (wallet) {
-        console.log(wallet)
         await this.client.connectToWallet(await wallet.getConnection())
-        await this.client.subscribeAddress('subscribe', 'current')
+        this.accounts = await this.client.subscribeAddress('subscribe', 'connected')
         this.pub = await this.client.address()
+        this.onAccount = this.pub
         this.balance = await this.client.getBalance(this.pub)
         this.walletName = this.client.rpcClient.info.name
+        this.addressResponse = await errorAsField(this.client.address())
+        this.heightResponse = await errorAsField(this.client.height())
+        this.nodeInfoResponse = await errorAsField(this.client.getNodeInfo())
+        this.compilerVersionResponse = await errorAsField(this.client.getCompilerVersion())
       },
       async scanForWallets () {
         const handleWallets = async function ({ wallets, newWallet }) {
-          
           newWallet = newWallet || Object.values(wallets)[0]
-          console.log(newWallet)
           if (confirm(`Do you want to connect to wallet ${newWallet.name}`)) {
             this.detector.stopScan()
+
             this.connectToWallet(newWallet)
           }
         }
+
+        const scannerConnection = await BrowserWindowMessageConnection({
+          connectionInfo: { id: 'spy' }
+        })
+        this.detector = await Detector({ connection: scannerConnection })
         this.detector.scan(handleWallets.bind(this))
       }
     },
@@ -273,36 +330,26 @@
       // Open iframe with Wallet if run in top window
       window !== window.parent || await this.getReverseWindow()
       //
-
-      let context = this
       this.client = await RpcAepp({
         name: 'AEPP',
         url: NODE_URL,
         internalUrl: NODE_INTERNAL_URL,
         compilerUrl: COMPILER_URL,
         onNetworkChange (params) {
-          console.log(params)
-          console.log(this.getNetworkId())
-          // if (this.getNetworkId() !== params.network) alert(`Connected network ${this.getNetworkId()} is not supported with wallet network ${params.netwok}`)
+          if (this.getNetworkId() !== params.networkId) alert(`Connected network ${this.getNetworkId()} is not supported with wallet network ${params.networkId}`)
         },
-        async onAddressChange (addresses) {
-          console.log("change")
-          console.log(await this.address())
-          context.pub = await this.address()
-          context.balance = await context.client.balance(context.pub).catch(console.log)
+        onAddressChange:  async (addresses) => {
+          this.pub = await this.client.address()
+          this.balance = await this.client.balance(this.pub).catch(e => '0')
+          this.addressResponse = await errorAsField(this.client.address())
         },
         onDisconnect (a) {
-          console.log(a)
-          // debugger
+          debugger
         }
       })
       this.height = await this.client.height()
-      const scannerConnection = await BrowserWindowMessageConnection({
-        connectionInfo: { id: 'spy' }
-      })
-      console.log(this.client)
-      this.detector = await Detector({ connection: scannerConnection })
-      console.log(this.detector)
+
+      // Start looking for wallets
       await this.scanForWallets()
     }
   }
